d860ba954592ca2466d12c608cd333bcbb02b858
Vishal Kumar <mailtovishal12030@gmail.com>
2023-07-22 01:04:22 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function togglemenu(){
+function toggleMenu() {
     const menu = document.querySelector(".menu-link");
     const icon = document.querySelector(".hamburger-icon");
-    
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
 }
